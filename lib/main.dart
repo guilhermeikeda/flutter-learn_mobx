d637dff64b922ffdeaebb2dd13beb554825b8915
Guilherme Carneiro Ikeda <guilherme@opus-software.com.br>
2020-11-16 20:43:28 -0300

    Add Dice

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:learn_mobx/screens/dice.screen.dart';
 
-import 'counter.dart';
-
-// Cria instancia
-final counter = Counter();
-
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Learn Mobx',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Learn MobX comigo'),
-      ),
-      body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text('Você apertou esse botão:'),
-              Observer(
-                builder: (_) => Text('${counter.value}',
-                    style: Theme.of(context).textTheme.headline4),
-              ),
-            ]),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: counter.increment,
-        tooltip: 'Incrementa',
-        child: Icon(Icons.add),
-      ),
+      home: DiceScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
---
+++ lib/stores/dice.store.dart
@@ -0,0 +1,33 @@
+import 'dart:math';
+import 'package:mobx/mobx.dart';
+
+part 'dice.store.g.dart';
+
+class DiceCounter = _DiceCounter with _$DiceCounter;
+
+abstract class _DiceCounter with Store {
+  static const int MAX_DICE_VALUE = 6;
+
+  @observable
+  int left = _randomDiceValue();
+
+  @observable
+  int right = _randomDiceValue();
+
+  @computed
+  int get total {
+    return left + right;
+  }
+
+  //int get total => left + right;
+
+  @action
+  roll() {
+    left = _randomDiceValue();
+    right = _randomDiceValue();
+  }
+
+  static int _randomDiceValue() {
+    return Random().nextInt(MAX_DICE_VALUE) + 1;
+  }
+}
